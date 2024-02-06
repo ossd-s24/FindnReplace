40febbd25b605f27b279df1d48ca963ed8b39471
samGreer <sjg9543@nyu.edu>
2024-02-05 22:04:34 -0500

    Added test js. Strings are currently hard coded, but the replacing of strings works

---
+++ ReplaceTest/replace.js
@@ -0,0 +1,4 @@
+const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT)
+  while(walker.nextNode()) {
+    walker.currentNode.nodeValue = walker.currentNode.nodeValue.replace("to", "dooby do!")
+  }
