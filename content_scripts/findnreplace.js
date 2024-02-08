ba95121c64244232d25c9d25fda53c7eeed9eea4
gboeker <glenda.boeker@gmail.com>
2024-02-08 13:10:03 -0500

    form and popup

--- content_scripts/findnreplace.js
+++
@@ -1,6 +0,0 @@
-
-const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT)
-
-while(walker.nextNode()) {
-  walker.currentNode.nodeValue = walker.currentNode.nodeValue.replace("to", "dooby do!")
-}
