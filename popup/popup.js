28c2587426ba82c87467cc0d6c59c6e78de96faf
samGreer <sjg9543@nyu.edu>
2024-02-08 14:36:31 -0500

    Added notes for multiple people to work at once

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,5 @@
+// NEEDS WORK: Will write to replacement.txt, line 1 will be the value from fins and line 2 will be value from replcae
+
 let form = document.getElementById("findReplaceForm");
 
 form.addEventListener("submit", (e) => {
